7b062a334a647bab2809b6a09f8cb9b3b0fb253e
Tien Mai <35861383+tienmai56@users.noreply.github.com>
2021-05-22 19:40:27 +0800

    Delete homework1 directory

--- homework1/question8.js
+++
@@ -1,9 +0,0 @@
-// Write a script that calculates the area of a square
-
-let length = Number(prompt("Enter side length of the square"));
-while(isNaN(length) || length < 0) {
-    length = Number(prompt("Invalid length. Please enter again"));
-}
-
-let area = length*length;
-alert("the square area is "+ area);
--- homework1/question9.js
+++
@@ -1,8 +0,0 @@
-// Write a script that calculates the area of a circle
-let radius = Number(prompt("Enter the radius of the circle"));
-while(isNaN(radius) || radius < 0) {
-    radius = Number(prompt("Invalid length. Please enter again"));
-}
-
-let area = Math.PI*radius*radius;
-alert("the circle area is "+ area);
